59a8af22974418dd848cc3d21ec47e06fbe92af0
Jens W. Klein <jk@kleinundpartner.at>
2019-11-19 11:03:27 +0100

    better name

--- src/collective/elastic/ingest/logging.py
+++ src/collective/elastic/ingest/logging.py
@@ -2,7 +2,7 @@
 try:
     import collective.elastic.plone  # noqa: W291
     import logging
-    logger = logging.getLogger("collective.es.ingest")
+    logger = logging.getLogger("collective.elastic.ingest")
 except ImportError:
     from celery.utils.log import get_task_logger
-    logger = get_task_logger("collective.es.ingest")
+    logger = get_task_logger("collective.elastic.ingest")
