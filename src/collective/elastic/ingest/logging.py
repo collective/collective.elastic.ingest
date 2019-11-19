eb86720f198915a8357f4b530bf65206c71110b1
Jens W. Klein <jk@kleinundpartner.at>
2019-11-19 11:00:46 +0100

    choose logger

--- src/collective/elastic/ingest/logging.py
+++ src/collective/elastic/ingest/logging.py
@@ -1,4 +1,8 @@
-from celery.utils.log import get_task_logger
-
-
-logger = get_task_logger("collective.es.ingest")
+# -*- coding: utf-8 -*-
+try:
+    import collective.elastic.plone  # noqa: W291
+    import logging
+    logger = logging.getLogger("collective.es.ingest")
+except ImportError:
+    from celery.utils.log import get_task_logger
+    logger = get_task_logger("collective.es.ingest")
