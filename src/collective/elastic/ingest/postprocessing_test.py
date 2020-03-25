372a0b3281a82add5b8d5b5f155ad3bafb32952f
Jens W. Klein <jk@kleinundpartner.at>
2020-03-25 15:48:53 +0100

    some fixes

--- src/collective/elastic/ingest/postprocessing_test.py
+++ src/collective/elastic/ingest/postprocessing_test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .ingest import postprocessing
+from . import postprocessing
 
 
 # ------------------------------------------------------------------------------
--- src/collective/elastic/ingest/preprocessing_test.py
+++ src/collective/elastic/ingest/preprocessing_test.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from .ingest import preprocessing
+from . import preprocessing
 
 
 # ------------------------------------------------------------------------------
