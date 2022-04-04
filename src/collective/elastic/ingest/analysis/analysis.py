88c9e7960b4ff1169e0f8e71f5bd021a9cefd951
ksuess <k.suess@rohberg.ch>
2022-04-04 09:21:11 +0200

    Create index with settings: analysis configuration.

--- src/collective/elastic/ingest/analysis/analysis.py
+++ src/collective/elastic/ingest/analysis/analysis.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """TODO
-- Recap if analysis settings should better be updated on Celery /Plone reboot,
-    but not on every _doc update.
+- Recap if analysis settings should better be updated on Celery /Plone reboot, but not on every _doc update.
 - Read lexicon from lexicon.txt
 """
 from ..elastic import get_ingest_client
@@ -9,6 +8,7 @@ from ..logging import logger
 
 import json
 import os
+import pprint
 
 _analysis_file = os.environ.get(
     "ANALYSIS_FILE", os.path.join(os.path.dirname(__file__), "analysis.json")
@@ -18,7 +18,7 @@ print("** Looking for _analysis_file", _analysis_file)
 try:
     with open(_analysis_file, mode="r") as fp:
         ANALYSISMAP = json.load(fp)
-except FileNotFoundError:
+except FileNotFoundError as e:
     ANALYSISMAP = None
 
 
@@ -29,27 +29,22 @@ def update_analysis(index_name):
     Overwrite with your analyzers by creating an ANALYSIS_FILE `analysis.json`.
     See README for details.
 
-    First `update_analysis` then `create_or_update_mapping`:
-    Mapping can use analyzers from analysis.json.
+    First `update_analysis` then `create_or_update_mapping`: Mapping can use analyzers from analysis.json.
     """
+    
     if ANALYSISMAP:
-        es = get_ingest_client()
-        if es is None:
-            logger.warning("No ElasticSearch client available.")
-            return
-
-        index_exists = es.indices.exists(index_name)
-        if not index_exists:
-            logger.warning("No ElasticSearch index available.")
-            return
-
-        # process analysis
         analysis_settings = ANALYSISMAP.get("settings", {})
         if analysis_settings:
-            logger.info("Update settings with analyzers.")
-            es.indices.close(index=index_name)
-            es.indices.put_settings(index=index_name, body=analysis_settings)
-            es.indices.open(index=index_name)
+            es = get_ingest_client()
+            if es is None:
+                logger.warning("No ElasticSearch client available.")
+                return
+            index_exists = es.indices.exists(index_name)
+            if index_exists:
+                return
+                
+            logger.info(f"Create index '{index_name}' with analysis settings from '{_analysis_file}', but without mapping.")
+            es.indices.create(index_name, body=ANALYSISMAP)
+            return
 
-    else:
-        logger.info("No analyzer configuration found.")
+    logger.info("No analyzer configuration found.")
