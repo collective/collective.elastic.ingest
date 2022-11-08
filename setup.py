52f9f1de31771da9bffb0bf79736a8b6eeaaf68b
Jens W. Klein <jk@kleinundpartner.at>
2022-11-08 10:37:44 +0100

    Back to development: 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="collective.elastic.ingest",
-    version="1.0",
+    version="1.1.dev0",
     project_urls={
         "PyPI": "https://pypi.python.org/pypi/collective.elastic.ingest",
         "Source": "https://github.com/collective/collective.elastic.ingest",
