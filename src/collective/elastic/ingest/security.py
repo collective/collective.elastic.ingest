829e7222f61c807e717a6204a0762eab5974dfc1
Katja Süss <k.suess@rohberg.ch>
2023-02-01 11:51:06 +0100

    Index allowedRolesAndUsers and section (primary path)

---
+++ src/collective/elastic/ingest/security.py
@@ -0,0 +1,10 @@
+from .logging import logger
+
+import os
+
+
+def enrichWithSecurityInfo(content):
+    content["allowedRolesAndUsers"] = content["@components"][
+        "component_allowedRolesAndUsers"
+    ]
+    return content
