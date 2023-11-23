17c6045d359b8669e057998e6aaa6c5b4b8377aa
Jens W. Klein <jk@kleinundpartner.at>
2023-11-23 16:52:32 +0100

    Remove check for elasticsearch/opensearch-py library version, we already pin this down in pyproject.toml

--- src/collective/elastic/ingest/__init__.py
+++ src/collective/elastic/ingest/__init__.py
@@ -1,19 +1,4 @@
-from importlib.metadata import version
-
 import os
 
 
 OPENSEARCH = os.environ.get("INDEX_OPENSEARCH") == "1"
-
-if OPENSEARCH:
-    version_opensearchpy = version("opensearch-py")
-    if int(version_opensearchpy[0]) < 2:
-        raise ValueError(
-            "opensearch-py 1.x is not supported, use version 1.x of the collective.elastic.ingest package."
-        )
-else:
-    version_elasticsearch = version("elasticsearch")
-    if int(version_elasticsearch[0]) < 7:
-        raise ValueError(
-            "elasticsearch < 7 is not supported, use Version 1.x of the collective.elastic.ingest package."
-        )
